74a181ee7335d8f3c6e051cad04219c4bb6564d3
Enrico Marino <enrico.marino@email.com>
2012-12-13 15:35:19 -0800

    added index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+/* !
+ * draggable
+ * Enable draggable functionality on any DOM element
+ *
+ * @author Enrico Marino and Federico Spini
+ * @copyright 2012 Enrico Marino and Federico Spini
+ * @license MIT
+ */
+
